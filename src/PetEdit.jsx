ffce5ab708e4aa4d7ea326c755792c9bee7e8be9
V1nicius28 <viniciusgarcezmachado@gmail.com>
2025-12-12 10:42:02 -0300

    feat(icons): add button icons

--- src/PetEdit.jsx
+++ src/PetEdit.jsx
@@ -1,4 +1,9 @@
-const PetItem = ({ pet, onEdit, onDelete }) => {
+import {
+  PencilSquareIcon,
+  TrashIcon
+} from "@heroicons/react/24/outline";
+
+const PetEdit = ({ pet, onEdit, onDelete }) => {
   return (
     <div className="bg-[#0b1739] text-white rounded-2xl p-4 shadow-lg flex justify-between items-center border border-[#343b4f] transition hover:shadow-2xl">
       <div>
@@ -12,14 +17,17 @@ const PetItem = ({ pet, onEdit, onDelete }) => {
       <div className="flex gap-2">
         <button
           onClick={() => onEdit(pet)}
-          className="bg-[#0038FF] hover:bg-[#1D4AEA] text-white font-semibold px-5 py-2 rounded-xl transition hover:scale-105 active:scale-95"
+          className="flex items-center gap-2 bg-[#0038FF] hover:bg-[#1D4AEA] text-white font-semibold px-5 py-2 rounded-xl transition hover:scale-105 active:scale-95"
         >
+          <PencilSquareIcon className="w-5 h-5" />
           Editar
         </button>
+
         <button
           onClick={() => onDelete(pet.id)}
-          className="bg-[#9E0004] hover:bg-[#B9030F] text-white font-semibold px-5 py-2 rounded-xl transition hover:scale-105 active:scale-95"
+          className="flex items-center gap-2 bg-[#9E0004] hover:bg-[#B9030F] text-white font-semibold px-5 py-2 rounded-xl transition hover:scale-105 active:scale-95"
         >
+          <TrashIcon className="w-5 h-5" />
           Deletar
         </button>
       </div>
@@ -27,5 +35,6 @@ const PetItem = ({ pet, onEdit, onDelete }) => {
   );
 };
 
-export default PetItem;
+export default PetEdit;
+
 
--- src/PetList.jsx
+++ src/PetList.jsx
@@ -1,18 +1,16 @@
-import PetItem from "./PetEdit";
+import PetEdit from "./PetEdit";
 
 const PetList = ({ pets, onEdit, onDelete }) => {
   return (
-    // WRAPPER → segura o border e faz o recorte com overflow-hidden
     <div className="rounded-2xl border border-[#343b4f] shadow-inner overflow-hidden bg-[#080F25]">
 
-      {/* ÁREA SCROLLÁVEL → fica dentro do wrapper */}
       <div className="p-6 space-y-4 max-h-[600px] overflow-auto custom-scrollbar">
         <h2 className="text-2xl font-semibold text-[#cb3cff] mb-4">
           Lista de Pets
         </h2>
 
         {pets.map((pet) => (
-          <PetItem key={pet.id} pet={pet} onEdit={onEdit} onDelete={onDelete} />
+          <PetEdit key={pet.id} pet={pet} onEdit={onEdit} onDelete={onDelete} />
         ))}
       </div>
 
